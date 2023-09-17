282c07d1056bf69f894f78a3009ef34e29c7e6a1
Alessandro Baldo <baldoalessandro@users.noreply.github.com>
2023-09-17 16:53:34 +0200

    Improves layout on mobile and larger screen
    - disables zooming
    - centers board on larger screen

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -6,43 +6,45 @@ import { Tile } from "./tile";
 import { useTilesetTheme } from "../graphics";
 
 export const Board: Component = () => {
-  let boardEl: HTMLDivElement | undefined;
+  let boardWrapperEl: HTMLDivElement | undefined;
 
   const { state } = useGameState();
   const { screenBgColor, boardBgColors, generateSprites } = useTilesetTheme();
 
   onMount(() => {
-    boardEl?.style.setProperty("--bg-color", screenBgColor);
-    boardEl?.style.setProperty("--tile-bg-1-color", boardBgColors[0]);
-    boardEl?.style.setProperty("--tile-bg-2-color", boardBgColors[1]);
+    boardWrapperEl?.style.setProperty("--bg-color", screenBgColor);
+    boardWrapperEl?.style.setProperty("--tile-bg-1-color", boardBgColors[0]);
+    boardWrapperEl?.style.setProperty("--tile-bg-2-color", boardBgColors[1]);
   });
 
   return (
-    <div ref={boardEl} class={cls.board}>
-      <svg xmlns="http://www.w3.org/2000/svg" class={cls.sprites}>
-        { generateSprites() }
-      </svg>
-      <main class={cls.tiles}>
-        <For each={state.tiles}>
-          {(tile, idx) => (
-            <Tile
-              tile={tile}
-              idx={idx()}
-              selected={state.selectedTile === idx()}
-            />
-          )}
-        </For>
-      </main>
-      <aside class={cls.score}>
-        <p>
-          <span>Current</span>
-          <output>{state.currentChain}</output>
-        </p>
-        <p>
-          <span>Highest</span>
-          <output>{state.highestChain}</output>
-        </p>
-      </aside>
+    <div ref={boardWrapperEl} class={cls.boardWrapper}>
+      <div class={cls.board}>
+        <svg xmlns="http://www.w3.org/2000/svg" class={cls.sprites}>
+          {generateSprites()}
+        </svg>
+        <main class={cls.tiles}>
+          <For each={state.tiles}>
+            {(tile, idx) => (
+              <Tile
+                tile={tile}
+                idx={idx()}
+                selected={state.selectedTile === idx()}
+              />
+            )}
+          </For>
+        </main>
+        <aside class={cls.score}>
+          <p>
+            <span>Current</span>
+            <output>{state.currentChain}</output>
+          </p>
+          <p>
+            <span>Highest</span>
+            <output>{state.highestChain}</output>
+          </p>
+        </aside>
+      </div>
     </div>
   );
 };
